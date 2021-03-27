940a6b9410d65d66756178ab6507d9eaedabcea7
Pablo L. Bispo <pablolbispo@pop-os.localdomain>
2021-03-27 11:21:21 -0300

    Adding cors

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from typing import Optional
+from typing import Optional, Dict
 
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -10,6 +21,8 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/ping")
+def ping_pong() -> Dict[str, str]:
+    return {
+        "ping": "pong"
+    }
