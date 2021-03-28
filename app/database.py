2d244e39bdeec759398b32292dd991e687254e06
Pablo L. Bispo <pablolbispo@pop-os.localdomain>
2021-03-28 20:17:34 -0300

    changing var name

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URI')
 
 
 engine = create_engine(
